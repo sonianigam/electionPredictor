e364910057163e2d2cf3d7e81d09e3bd1b8b93ab
Sonia Nigam <sonianigam2017@u.northwestern.edu>
2018-10-12 14:21:52 -0700

    Migrate to python3 and update logic to be party based vs candidate based

--- candidate_predictor.py
+++ candidate_predictor.py
@@ -5,26 +5,26 @@ import string
 import sys
 
 def main():
-    twitter_handle = raw_input("Please enter your twitter handle: ")
+    twitter_handle = input("Please enter your twitter handle: ")
 
     tweets = setup.tweets
-    twitter_stopwords = ["rt"]
+    twitter_stopwords = ["rt", "#tcot", "’", "…", "“", "..."]
     punctuation = [c for c in string.punctuation]
 
-    for candidate in tweets:
-        content = itertools.chain.from_iterable(tweets[candidate])
+    for party in tweets:
+        print(party)
+        content = itertools.chain.from_iterable(tweets[party])
         stopwords = nltk.corpus.stopwords.words('english') + punctuation + twitter_stopwords
         content = [word.lower() for word in content]
         filtered_content = [word for word in content if word not in stopwords]
 
         freq_dist = nltk.FreqDist(filtered_content)
+        print(freq_dist.most_common(10))
 
 def get_user_tweets(twitter_handle):
     auth = tweepy.OAuthHandler(config.consumer_key, config.secret_consumer_key)
     auth.set_access_token(config.access_token, config.secret_access_token)
     api = tweepy.API(auth)
-
-
 
 if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import tweepy
 import math
-import cPickle as pickle
+import pickle
 import pdb
 from nltk.tokenize.casual import TweetTokenizer
 import config
@@ -9,19 +9,19 @@ auth = tweepy.OAuthHandler(config.consumer_key, config.secret_consumer_key)
 auth.set_access_token(config.access_token, config.secret_access_token)
 api = tweepy.API(auth)
 
-accounts = {"hillary": ["HillaryClinton", "johnlegend", "kittukolluri", "Clanglotz"], "trump": ["realDonaldTrump", "EdButowsky", "taylorjackson02", "ben_jammin002"], "bernie": ["BernieSanders", "Kaepernick7", "abzabec"]}
+accounts = {"democrat": ["HillaryClinton","kittukolluri", "Clanglotz", "stevendcoffey", "ezraklein", "ParkerMolloy", "TamikaDMallory" ], "republican": ["realDonaldTrump", "EdButowsky", "taylorjackson02", "ben_jammin002", "AppSame", "michellemalkin", "FredThompson"]}
 
-tweets = {"hillary": [], "trump": [], "bernie": []}
+tweets = {"democrat": [], "republican": []}
 
 try:
     tweets = pickle.load(open("tweets.pickle", "rb"))
 except (OSError, IOError) as e:
     tokenizer = TweetTokenizer(preserve_case=False, reduce_len=True)
-    for candidate in accounts:
-        supporters = accounts[candidate]
-        number = math.floor(1000/len(supporters))
+    for party in accounts:
+        supporters = accounts[party]
+        number = math.floor(2000/len(supporters))
         for user in supporters:
             user_tweets = api.user_timeline(user, count = number)
-            tweets[candidate].extend([tokenizer.tokenize(t.text.encode("utf-8")) for t in user_tweets])
+            tweets[party].extend([tokenizer.tokenize(t.text.encode("utf-8")) for t in user_tweets])
 
     pickle.dump(tweets, open("tweets.pickle", "wb"))
